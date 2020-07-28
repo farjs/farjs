8de257d8ee56c281d417fe57dfc6e45517cf844d
Viktor Podzigun <viktor.podzigun@gmail.com>
2020-07-28 21:28:05 +0200

    Added npm package build configuration, fixed #3

---
+++ app/rollup.config.js
@@ -0,0 +1,29 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import resolve from 'rollup-plugin-node-resolve'
+
+import pkg from './package.json'
+
+export default {
+  input: './index.js',
+  output: [
+    {
+      file: './dist/far.js',
+      format: 'cjs',
+      sourcemap: false
+    }
+  ],
+  external: Object.keys(pkg.dependencies).concat([
+    'fs', 'path'
+  ]),
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve(),
+    commonjs({
+      ignore: Object.keys(pkg.dependencies)
+    })
+  ]
+}
--- project/src/main/scala/definitions/FarjsApp.scala
+++ project/src/main/scala/definitions/FarjsApp.scala
@@ -30,20 +30,77 @@ object FarjsApp extends ScalaJsModule {
       webpackBundlingMode := BundlingMode.LibraryOnly(),
       version in webpack := "4.29.0",
 
-      //useYarn := true,
-      //yarnExtraArgs := Seq("--frozen-lockfile"),
-      
-      npmDevDependencies in Compile ++= Seq(
-        "webpack-merge" -> "4.1.0",
-        "webpack-node-externals" -> "1.7.2"
-      ),
-
       //dev
       webpackConfigFile in fastOptJS := Some(baseDirectory.value / "dev.webpack.config.js"),
       //prod
-      webpackConfigFile in fullOptJS := Some(baseDirectory.value / "dev.webpack.config.js")
+      webpackConfigFile in fullOptJS := Some(baseDirectory.value / "dev.webpack.config.js"),
       //reload workflow and tests
-      //webpackConfigFile in Test := Some(baseDirectory.value / "test.webpack.config.js")
+      //webpackConfigFile in Test := Some(baseDirectory.value / "test.webpack.config.js"),
+
+      //useYarn := true,
+      //yarnExtraArgs := Seq("--frozen-lockfile"),
+
+      npmDevDependencies in Compile ++= Seq(
+        "webpack-merge" -> "4.1.0",
+        "webpack-node-externals" -> "1.7.2",
+
+        "@babel/cli" -> "^7.0.0",
+        "@babel/core" -> "^7.0.0",
+        "@babel/plugin-proposal-class-properties" -> "^7.7.0",
+        "@babel/plugin-proposal-object-rest-spread" -> "^7.0.0",
+        "@babel/plugin-transform-flow-strip-types" -> "^7.0.0",
+        "@babel/preset-env" -> "^7.0.0",
+        "@babel/preset-react" -> "^7.0.0",
+        "@babel/register" -> "^7.0.0",
+        "rollup" -> "^0.65.0",
+        "rollup-plugin-babel" -> "^4.0.2",
+        "rollup-plugin-commonjs" -> "^9.1.6",
+        "rollup-plugin-node-resolve" -> "^3.3.0",
+        "rollup-plugin-peer-deps-external" -> "^2.2.0"
+      ),
+
+      additionalNpmConfig in Compile := {
+        import scalajsbundler.util.JSON._
+        Map(
+          "name" -> str("far.js"),
+          "version" -> str(version.value),
+          "description" -> str("File and Archive Manager (FAR) app built with Scala.js/React.js and runs on Node.js"),
+          "scripts" -> obj(
+            "build" -> str("rollup -c")
+          ),
+          "repository" -> obj(
+            "type" -> str("git"),
+            "url" -> str("git+https://github.com/scommons/far-js.git")
+          ),
+          "bugs" -> obj(
+            "url" -> str("https://github.com/scommons/far-js/issues")
+          ),
+          "homepage" -> str("https://github.com/scommons/far-js#readme"),
+          "browserslist" -> str("maintained node versions"),
+          "private" -> bool(false),
+          "license" -> str("MIT"),
+          "author" -> str("viktor-podzigun"),
+          "keywords" -> arr(
+            str("farmanager"),
+            str("scalajs"),
+            str("reactjs"),
+            str("scala"),
+            str("scala-js"),
+            str("blessed"),
+            str("react-blessed"),
+            str("react"),
+            str("terminal"),
+            str("file-manager"),
+            str("filemanager"),
+            str("filemanager-ui"),
+            str("filemanagement"),
+            str("cli")
+          ),
+          "files" -> arr(
+            str("dist/far.js")
+          )
+        )
+      }
     )
 
   override val internalDependencies: Seq[ClasspathDep[ProjectReference]] = Seq(
