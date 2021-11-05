5efa3f8acbd0e20d5f8cd67129074713f92dacf7
Viktor Podzigun <viktor.podzigun@gmail.com>
2021-11-05 12:19:45 +0100

    Show file exists warning when move within same drive, fixed #42

--- filelist/src/main/scala/farjs/filelist/copy/MoveItems.scala
+++ filelist/src/main/scala/farjs/filelist/copy/MoveItems.scala
@@ -3,7 +3,8 @@ package farjs.filelist.copy
 import farjs.filelist.FileListActions
 import farjs.filelist.FileListActions.FileListTaskAction
 import farjs.filelist.api.FileListItem
-import farjs.ui.popup.{StatusPopup, StatusPopupProps}
+import farjs.ui.popup._
+import farjs.ui.theme.Theme
 import io.github.shogowada.scalajs.reactjs.redux.Redux.Dispatch
 import scommons.nodejs.{FS, path}
 import scommons.react._
@@ -11,7 +12,7 @@ import scommons.react.hooks._
 import scommons.react.redux.task.FutureTask
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{Future, Promise}
 import scala.util.{Failure, Success}
 
 case class MoveItemsProps(dispatch: Dispatch,
@@ -25,24 +26,39 @@ case class MoveItemsProps(dispatch: Dispatch,
 object MoveItems extends FunctionComponent[MoveItemsProps] {
 
   private[copy] var statusPopupComp: UiComponent[StatusPopupProps] = StatusPopup
+  private[copy] var messageBoxComp: UiComponent[MessageBoxProps] = MessageBox
   private[copy] var fs: FS = scommons.nodejs.fs
 
+  private case class MoveState(currItem: String = "",
+                               existing: Option[String] = None)
+
   protected def render(compProps: Props): ReactElement = {
-    val (currItem, setCurrItem) = useState("")
+    val (state, setState) = useStateUpdater(() => MoveState())
     val inProgress = useRef(false)
+    val existsPromise = useRef(Promise.successful[Boolean](true))
+    val askWhenExists = useRef(true)
     val props = compProps.wrapped
 
     def moveItems(): Unit = {
       val resultF = props.items.foldLeft(Future.successful(true)) { (resF, currItem) =>
         resF.flatMap {
           case true if inProgress.current =>
-            setCurrItem(currItem.name)
-            fs.rename(
-              oldPath = path.join(props.fromPath, currItem.name),
-              newPath = path.join(props.toPath, currItem.name)
-            ).map { _ =>
-              props.onTopItem(currItem)
-              inProgress.current
+            setState(_.copy(currItem = currItem.name))
+            val oldPath = path.join(props.fromPath, currItem.name)
+            val newPath = path.join(props.toPath, currItem.name)
+            val exists = !currItem.isDir && fs.existsSync(newPath)
+
+            if (exists && askWhenExists.current) {
+              setState(_.copy(existing = Some(newPath)))
+              existsPromise.current = Promise[Boolean]()
+            }
+            existsPromise.current.future.flatMap { overwrite =>
+              if (!exists || overwrite) {
+                fs.rename(oldPath, newPath).map { _ =>
+                  props.onTopItem(currItem)
+                  inProgress.current
+                }
+              } else Future.successful(inProgress.current)
             }
           case res => Future.successful(res)
         }
@@ -55,6 +71,16 @@ object MoveItems extends FunctionComponent[MoveItemsProps] {
       }
     }
 
+    def onExistsAction(overwrite: Boolean,
+                       all: Boolean = false,
+                       cancel: Boolean = false): () => Unit = { () =>
+      
+      setState(_.copy(existing = None))
+      askWhenExists.current = !all
+      inProgress.current = !cancel
+      existsPromise.current.trySuccess(overwrite)
+    }
+
     useLayoutEffect({ () =>
       // start
       inProgress.current = true
@@ -62,14 +88,35 @@ object MoveItems extends FunctionComponent[MoveItemsProps] {
       ()
     }, Nil)
 
-    <(statusPopupComp())(^.wrapped := StatusPopupProps(
-      text = s"Moving item\n$currItem",
-      title = "Move",
-      closable = true,
-      onClose = { () =>
-        // stop
-        inProgress.current = false
+    <.>()(
+      <(statusPopupComp())(^.wrapped := StatusPopupProps(
+        text = s"Moving item\n${state.currItem}",
+        title = "Move",
+        closable = true,
+        onClose = { () =>
+          // stop
+          inProgress.current = false
+        }
+      ))(),
+
+      state.existing.map { existing =>
+        <(messageBoxComp())(^.wrapped := MessageBoxProps(
+          title = "Warning",
+          message = s"File already exists.\nDo you want to overwrite it's content?\n\n$existing",
+          actions = List(
+            MessageBoxAction("Overwrite", onExistsAction(overwrite = true)),
+            MessageBoxAction("All", onExistsAction(overwrite = true, all = true)),
+            MessageBoxAction("Skip", onExistsAction(overwrite = false)),
+            MessageBoxAction("Skip all", onExistsAction(overwrite = false, all = true)),
+            MessageBoxAction(
+              label = "Cancel",
+              onAction = onExistsAction(overwrite = false, all = true, cancel = true),
+              triggeredOnClose = true
+            )
+          ),
+          style = Theme.current.popup.error
+        ))()
       }
-    ))()
+    )
   }
 }
--- filelist/src/test/scala/farjs/filelist/copy/MoveItemsSpec.scala
+++ filelist/src/test/scala/farjs/filelist/copy/MoveItemsSpec.scala
@@ -4,7 +4,8 @@ import farjs.filelist.FileListActions.FileListTaskAction
 import farjs.filelist._
 import farjs.filelist.api.FileListItem
 import farjs.filelist.copy.MoveItems._
-import farjs.ui.popup.StatusPopupProps
+import farjs.ui.popup.{MessageBoxProps, StatusPopupProps}
+import farjs.ui.theme.Theme
 import org.scalatest.Succeeded
 import scommons.nodejs.test.AsyncTestSpec
 import scommons.nodejs.{FS, path}
@@ -16,6 +17,7 @@ import scala.concurrent.{Future, Promise}
 class MoveItemsSpec extends AsyncTestSpec with BaseTestSpec with TestRendererUtils {
 
   MoveItems.statusPopupComp = () => "StatusPopup".asInstanceOf[ReactClass]
+  MoveItems.messageBoxComp = () => "MessageBox".asInstanceOf[ReactClass]
 
   it should "call onTopItem/onDone when success" in {
     //given
@@ -41,6 +43,7 @@ class MoveItemsSpec extends AsyncTestSpec with BaseTestSpec with TestRendererUti
       }
     }.flatMap { _ =>
       //then
+      (fs.existsSync _).expects(path.join(props.toPath, "file 1")).returning(false)
       (fs.rename _).expects(path.join(props.fromPath, "file 1"), path.join(props.toPath, "file 1")).returning(p.future)
       onTopItem.expects(item1)
       onTopItem.expects(item2)
@@ -93,7 +96,7 @@ class MoveItemsSpec extends AsyncTestSpec with BaseTestSpec with TestRendererUti
     }
   }
 
-  it should "handle cancel action" in {
+  it should "handle onClose action" in {
     //given
     val onTopItem = mockFunction[FileListItem, Unit]
     val onDone = mockFunction[Unit]
@@ -128,6 +131,270 @@ class MoveItemsSpec extends AsyncTestSpec with BaseTestSpec with TestRendererUti
 
       //when
       p.success(())
+      eventually {
+        done shouldBe true
+      }
+    }
+  }
+
+  it should "render item exists message and handle Cancel action" in {
+    //given
+    val onTopItem = mockFunction[FileListItem, Unit]
+    val onDone = mockFunction[Unit]
+    val dispatch = mockFunction[Any, Any]
+    val actions = mock[FileListActions]
+    val fs = mock[FS]
+    MoveItems.fs = fs
+    val item1 = FileListItem("file 1", size = 1)
+    val item2 = FileListItem("file 2", size = 10)
+    val props = MoveItemsProps(dispatch, actions, "/from/path", List(item1, item2), "/to/path", onTopItem, onDone)
+    val newPath = path.join(props.toPath, "file 1")
+    (fs.existsSync _).expects(newPath).returning(true)
+    val renderer = createTestRenderer(<(MoveItems())(^.wrapped := props)())
+
+    eventually {
+      inside(renderer.root.children.toList) { case List(_, existsMessage) =>
+        assertTestComponent(existsMessage, messageBoxComp) {
+          case MessageBoxProps(title, message, resActions, style) =>
+            title shouldBe "Warning"
+            message shouldBe s"File already exists.\nDo you want to overwrite it's content?\n\n$newPath"
+            inside(resActions) { case List(overwrite, all, skip, skipAll, cancel) =>
+              overwrite.label shouldBe "Overwrite"
+              all.label shouldBe "All"
+              skip.label shouldBe "Skip"
+              skipAll.label shouldBe "Skip all"
+              cancel.label shouldBe "Cancel"
+              cancel.triggeredOnClose shouldBe true
+            }
+            style shouldBe Theme.current.popup.error
+        }
+      }
+    }.flatMap { _ =>
+      //then
+      var done = false
+      onDone.expects().onCall { () =>
+        done = true
+      }
+
+      //when
+      findComponentProps(renderer.root, messageBoxComp).actions.last.onAction()
+
+      //then
+      findProps(renderer.root, messageBoxComp) should be (empty)
+      eventually {
+        done shouldBe true
+      }
+    }
+  }
+
+  it should "render item exists message and handle Overwrite action" in {
+    //given
+    val onTopItem = mockFunction[FileListItem, Unit]
+    val onDone = mockFunction[Unit]
+    val dispatch = mockFunction[Any, Any]
+    val actions = mock[FileListActions]
+    val fs = mock[FS]
+    MoveItems.fs = fs
+    val item1 = FileListItem("file 1", size = 1)
+    val item2 = FileListItem("file 2", size = 10)
+    val props = MoveItemsProps(dispatch, actions, "/from/path", List(item1, item2), "/to/path", onTopItem, onDone)
+    val newPath1 = path.join(props.toPath, "file 1")
+    (fs.existsSync _).expects(newPath1).returning(true)
+    val renderer = createTestRenderer(<(MoveItems())(^.wrapped := props)())
+
+    eventually {
+      inside(renderer.root.children.toList) { case List(_, existsMessage) =>
+        assertTestComponent(existsMessage, messageBoxComp) {
+          case MessageBoxProps(_, message, _, _) =>
+            message shouldBe s"File already exists.\nDo you want to overwrite it's content?\n\n$newPath1"
+        }
+      }
+    }.flatMap { _ =>
+      //then
+      (fs.rename _).expects(path.join(props.fromPath, "file 1"), newPath1).returning(Future.unit)
+      onTopItem.expects(item1)
+      
+      //when
+      findComponentProps(renderer.root, messageBoxComp).actions.head.onAction()
+      
+      //then
+      findProps(renderer.root, messageBoxComp) should be (empty)
+      val newPath2 = path.join(props.toPath, "file 2")
+      (fs.existsSync _).expects(newPath2).returning(true)
+
+      eventually {
+        inside(renderer.root.children.toList) { case List(_, existsMessage) =>
+          assertTestComponent(existsMessage, messageBoxComp) {
+            case MessageBoxProps(_, message, _, _) =>
+              message shouldBe s"File already exists.\nDo you want to overwrite it's content?\n\n$newPath2"
+          }
+        }
+      }.flatMap { _ =>
+        //then
+        (fs.rename _).expects(path.join(props.fromPath, "file 2"), newPath2).returning(Future.unit)
+        onTopItem.expects(item2)
+        
+        var done = false
+        onDone.expects().onCall { () =>
+          done = true
+        }
+
+        //when
+        findComponentProps(renderer.root, messageBoxComp).actions.head.onAction()
+
+        //then
+        findProps(renderer.root, messageBoxComp) should be (empty)
+        eventually {
+          done shouldBe true
+        }
+      }
+    }
+  }
+
+  it should "render item exists message and handle All action" in {
+    //given
+    val onTopItem = mockFunction[FileListItem, Unit]
+    val onDone = mockFunction[Unit]
+    val dispatch = mockFunction[Any, Any]
+    val actions = mock[FileListActions]
+    val fs = mock[FS]
+    MoveItems.fs = fs
+    val item1 = FileListItem("file 1", size = 1)
+    val item2 = FileListItem("file 2", size = 10)
+    val props = MoveItemsProps(dispatch, actions, "/from/path", List(item1, item2), "/to/path", onTopItem, onDone)
+    val newPath1 = path.join(props.toPath, "file 1")
+    (fs.existsSync _).expects(newPath1).returning(true)
+    val renderer = createTestRenderer(<(MoveItems())(^.wrapped := props)())
+
+    eventually {
+      inside(renderer.root.children.toList) { case List(_, existsMessage) =>
+        assertTestComponent(existsMessage, messageBoxComp) {
+          case MessageBoxProps(_, message, _, _) =>
+            message shouldBe s"File already exists.\nDo you want to overwrite it's content?\n\n$newPath1"
+        }
+      }
+    }.flatMap { _ =>
+      //then
+      (fs.rename _).expects(path.join(props.fromPath, "file 1"), newPath1).returning(Future.unit)
+      onTopItem.expects(item1)
+      
+      val newPath2 = path.join(props.toPath, "file 2")
+      (fs.existsSync _).expects(newPath2).returning(true)
+      (fs.rename _).expects(path.join(props.fromPath, "file 2"), newPath2).returning(Future.unit)
+      onTopItem.expects(item2)
+      
+      var done = false
+      onDone.expects().onCall { () =>
+        done = true
+      }
+      
+      //when
+      findComponentProps(renderer.root, messageBoxComp).actions(1).onAction()
+      
+      //then
+      findProps(renderer.root, messageBoxComp) should be (empty)
+      eventually {
+        done shouldBe true
+      }
+    }
+  }
+
+  it should "render item exists message and handle Skip action" in {
+    //given
+    val onTopItem = mockFunction[FileListItem, Unit]
+    val onDone = mockFunction[Unit]
+    val dispatch = mockFunction[Any, Any]
+    val actions = mock[FileListActions]
+    val fs = mock[FS]
+    MoveItems.fs = fs
+    val item1 = FileListItem("file 1", size = 1)
+    val item2 = FileListItem("file 2", size = 10)
+    val props = MoveItemsProps(dispatch, actions, "/from/path", List(item1, item2), "/to/path", onTopItem, onDone)
+    val newPath1 = path.join(props.toPath, "file 1")
+    (fs.existsSync _).expects(newPath1).returning(true)
+    val renderer = createTestRenderer(<(MoveItems())(^.wrapped := props)())
+
+    eventually {
+      inside(renderer.root.children.toList) { case List(_, existsMessage) =>
+        assertTestComponent(existsMessage, messageBoxComp) {
+          case MessageBoxProps(_, message, _, _) =>
+            message shouldBe s"File already exists.\nDo you want to overwrite it's content?\n\n$newPath1"
+        }
+      }
+    }.flatMap { _ =>
+      //when
+      findComponentProps(renderer.root, messageBoxComp).actions(2).onAction()
+
+      //then
+      findProps(renderer.root, messageBoxComp) should be (empty)
+      val newPath2 = path.join(props.toPath, "file 2")
+      (fs.existsSync _).expects(newPath2).returning(true)
+
+      eventually {
+        inside(renderer.root.children.toList) { case List(_, existsMessage) =>
+          assertTestComponent(existsMessage, messageBoxComp) {
+            case MessageBoxProps(_, message, _, _) =>
+              message shouldBe s"File already exists.\nDo you want to overwrite it's content?\n\n$newPath2"
+          }
+        }
+      }.flatMap { _ =>
+        var done = false
+        onDone.expects().onCall { () =>
+          done = true
+        }
+
+        //when
+        findComponentProps(renderer.root, messageBoxComp).actions(2).onAction()
+
+        //then
+        findProps(renderer.root, messageBoxComp) should be (empty)
+        eventually {
+          done shouldBe true
+        }
+      }
+    }
+  }
+
+  it should "render item exists message and handle Skip all action" in {
+    //given
+    val onTopItem = mockFunction[FileListItem, Unit]
+    val onDone = mockFunction[Unit]
+    val dispatch = mockFunction[Any, Any]
+    val actions = mock[FileListActions]
+    val fs = mock[FS]
+    MoveItems.fs = fs
+    val item1 = FileListItem("file 1", size = 1)
+    val item2 = FileListItem("file 2", size = 10)
+    val item3 = FileListItem("file 3", size = 11)
+    val props = MoveItemsProps(dispatch, actions, "/from/path", List(item1, item2, item3), "/to/path", onTopItem, onDone)
+    val newPath1 = path.join(props.toPath, "file 1")
+    (fs.existsSync _).expects(newPath1).returning(true)
+    val renderer = createTestRenderer(<(MoveItems())(^.wrapped := props)())
+
+    eventually {
+      inside(renderer.root.children.toList) { case List(_, existsMessage) =>
+        assertTestComponent(existsMessage, messageBoxComp) {
+          case MessageBoxProps(_, message, _, _) =>
+            message shouldBe s"File already exists.\nDo you want to overwrite it's content?\n\n$newPath1"
+        }
+      }
+    }.flatMap { _ =>
+      (fs.existsSync _).expects(path.join(props.toPath, "file 2")).returning(true)
+      val newPath3 = path.join(props.toPath, "file 3")
+      (fs.existsSync _).expects(newPath3).returning(false)
+      (fs.rename _).expects(path.join(props.fromPath, "file 3"), newPath3).returning(Future.unit)
+      onTopItem.expects(item3)
+
+      var done = false
+      onDone.expects().onCall { () =>
+        done = true
+      }
+
+      //when
+      findComponentProps(renderer.root, messageBoxComp).actions(3).onAction()
+
+      //then
+      findProps(renderer.root, messageBoxComp) should be (empty)
       eventually {
         done shouldBe true
       }
