ddb981ab433d025d7871db4774a4b5be3af4ba65
Viktor Podzigun <viktor.podzigun@gmail.com>
2022-03-17 17:27:43 +0100

    Support short datetime parsing

--- app/src/main/scala/farjs/app/util/DateTimeUtil.scala
+++ app/src/main/scala/farjs/app/util/DateTimeUtil.scala
@@ -4,13 +4,40 @@ import scala.scalajs.js
 
 object DateTimeUtil {
 
-  private lazy val dateTimeRegex = """(\d{2})-(\d{2})-(\d{4}) (\d{2}):(\d{2})""".r
+  private val currYear = new js.Date().getFullYear().toInt
+  
+  private lazy val mmddyyyyTimeRegex = """(\d{2})-(\d{2})-(\d{4}) (\d{2}):(\d{2})""".r
+  private lazy val ddmmyyTimeRegex = """(\d{2}).(\d{2}).(\d{2}) (\d{2}):(\d{2})""".r
 
   def parseDateTime(input: String): Double = {
-    (for {
-      dateTimeRegex(month, date, year, hours, minutes) <- dateTimeRegex.findFirstMatchIn(input)
-    } yield {
-      new js.Date(year.toInt, month.toInt - 1, date.toInt, hours.toInt, minutes.toInt).getTime()
-    }).getOrElse(0.0)
+    
+    def mmddyyyyTime: Option[(Int, Int, Int, Int, Int)] =
+      for {
+        mmddyyyyTimeRegex(month, date, year, hours, minutes) <- mmddyyyyTimeRegex.findFirstMatchIn(input)
+      } yield {
+        (year.toInt, month.toInt, date.toInt, hours.toInt, minutes.toInt)
+      }
+
+    def ddmmyyTime: Option[(Int, Int, Int, Int, Int)] =
+      for {
+        ddmmyyTimeRegex(date, month, year, hours, minutes) <- ddmmyyTimeRegex.findFirstMatchIn(input)
+      } yield {
+        (year.toInt, month.toInt, date.toInt, hours.toInt, minutes.toInt)
+      }
+
+    mmddyyyyTime
+      .orElse(ddmmyyTime)
+      .map { case (year, month, date, hours, minutes) =>
+        val fullYear =
+          if (year < 100) {
+            val after2000 = year + 2000
+            if (after2000 > currYear) year + 1900
+            else after2000
+          }
+          else year
+          
+        new js.Date(fullYear, month - 1, date, hours, minutes).getTime()
+      }
+      .getOrElse(0.0)
   }
 }
--- app/src/test/scala/farjs/app/util/DateTimeUtilSpec.scala
+++ app/src/test/scala/farjs/app/util/DateTimeUtilSpec.scala
@@ -10,5 +10,7 @@ class DateTimeUtilSpec extends TestSpec {
   it should "parse date and time when parseDateTime" in {
     //when & then
     parseDateTime("06-28-2019 16:09") shouldBe js.Date.parse("2019-06-28T16:09:00")
+    parseDateTime("28.06.19 16:09") shouldBe js.Date.parse("2019-06-28T16:09:00")
+    parseDateTime("28.06.99 16:09") shouldBe js.Date.parse("1999-06-28T16:09:00")
   }
 }
