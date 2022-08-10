ff8562b54e84b1d7384541bec990d224a8326305
Viktor Podzigun <viktor.podzigun@gmail.com>
2022-08-10 20:54:26 +0200

    Disabled version check on startup (#51)

--- app/src/main/resources/bin/farjs.js
+++ app/src/main/resources/bin/farjs.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node --title=FAR.js
 
-const versionChecker = require('../dist/versionChecker.js')
-versionChecker.check(() => {
+// const versionChecker = require('../dist/versionChecker.js')
+// versionChecker.check(() => {
 
   require('../farjs-app-opt-bundle.js').FarjsApp.start()
-})
+// })
