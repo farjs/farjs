4755c68d3f7ee8ce091113b9936432c6167afb84
Viktor Podzigun <viktor.podzigun@gmail.com>
2022-12-24 13:39:30 +0100

    Added FSServices/FolderShortcutsService interfaces (#61)

---
+++ app/src/main/scala/farjs/app/filelist/fs/popups/FolderShortcutsService.scala
@@ -0,0 +1,12 @@
+package farjs.app.filelist.fs.popups
+
+import scala.concurrent.Future
+
+trait FolderShortcutsService {
+
+  def getAll: Future[Seq[Option[String]]]
+
+  def save(index: Int, path: String): Future[Unit]
+
+  def delete(index: Int): Future[Unit]
+}
