72c4dc4235ec08c4df4934bf20be185842f74141
Viktor Podzigun <viktor.podzigun@gmail.com>
2023-07-24 16:35:43 +0200

    Extracted UI.splitText

--- ui/src/main/scala/farjs/ui/UI.scala
+++ ui/src/main/scala/farjs/ui/UI.scala
@@ -1,31 +1,17 @@
 package farjs.ui
 
-import farjs.ui.raw.NativeUI
 import scommons.react.blessed.BlessedStyle
 
-object UI {
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
 
-  def renderText2(style: BlessedStyle, text: String): String =
-    NativeUI.renderText2(style, text)
+@js.native
+@JSImport("@farjs/ui/UI.mjs", JSImport.Namespace)
+object UI extends js.Object {
 
-  def renderText(isBold: Boolean, fgColor: String, bgColor: String, text: String): String =
-    NativeUI.renderText(isBold, fgColor, bgColor, text)
+  def renderText2(style: BlessedStyle, text: String): String = js.native
 
-  def splitText(text: String, maxLen: Int): List[String] = {
-    val sentences = text.split('\n')
-    
-    sentences.toList.flatMap { sentence =>
-      val words = sentence.trim.split(' ')
-
-      words.foldLeft(List.empty[String]) {
-        case (Nil, item) => List(item)
-        case (head :: tail, item) =>
-          if ((head.length + item.length + 1) > maxLen) {
-            item :: head :: tail
-          } else {
-            s"$head $item" :: tail
-          }
-      }.reverse
-    }
-  }
+  def renderText(isBold: Boolean, fgColor: String, bgColor: String, text: String): String = js.native
+  
+  def splitText(text: String, maxLen: Int): js.Array[String] = js.native
 }
