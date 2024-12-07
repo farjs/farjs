7d1887ae5970cfd55401297743bca589aa966bf2
Viktor Podzigun <viktor.podzigun@gmail.com>
2024-12-07 19:26:21 +0100

    Removed MockPanelStack and PanelStackSpec

--- viewer/src/test/scala/farjs/viewer/quickview/QuickViewPluginSpec.scala
+++ viewer/src/test/scala/farjs/viewer/quickview/QuickViewPluginSpec.scala
@@ -1,27 +1,14 @@
 package farjs.viewer.quickview
 
 import farjs.filelist.stack._
+import org.scalatest.OptionValues
 import scommons.nodejs.test.AsyncTestSpec
 import scommons.react.ReactClass
 
 import scala.scalajs.js
 
-class QuickViewPluginSpec extends AsyncTestSpec {
+class QuickViewPluginSpec extends AsyncTestSpec with OptionValues {
   
-  //noinspection TypeAnnotation
-  class Stack(isActive: Boolean = false) {
-    val push = mockFunction[PanelStackItem[QuickViewParams], Unit]
-    val pop = mockFunction[Unit]
-    val peek = mockFunction[PanelStackItem[QuickViewParams]]
-
-    val stack = new MockPanelStack[QuickViewParams](
-      isActive = isActive,
-      pushMock = push,
-      popMock = pop,
-      peekMock = peek
-    )
-  }
-
   it should "define triggerKeys" in {
     //when & then
     QuickViewPlugin.triggerKeys.toList shouldBe List("C-q")
@@ -29,66 +16,89 @@ class QuickViewPluginSpec extends AsyncTestSpec {
   
   it should "remove plugin from left panel when onKeyTrigger" in {
     //given
-    val leftStack = new Stack(isActive = true)
-    val rightStack = new Stack
-    val stacks = PanelStacks(PanelStackData(leftStack.stack, null), PanelStackData(rightStack.stack, null))
-    leftStack.peek.expects().returning(PanelStackItem(QuickViewPanel()))
-    
-    //then
-    leftStack.pop.expects()
+    var stackState = js.Array[PanelStackItem[_]](
+      PanelStackItem(QuickViewPanel(), js.undefined, js.undefined, QuickViewParams()),
+      PanelStackItem("fsComp".asInstanceOf[ReactClass], js.undefined, js.undefined, js.undefined)
+    )
+    val leftStack = new PanelStack(isActive = true, stackState, { f =>
+      stackState = f(stackState)
+    }: js.Function1[js.Array[PanelStackItem[_]], js.Array[PanelStackItem[_]]] => Unit)
+    val rightStack = new PanelStack(isActive = false, js.Array(
+      PanelStackItem("fsComp".asInstanceOf[ReactClass], js.undefined, js.undefined, js.undefined)
+    ), null)
+    val stacks = PanelStacks(PanelStackData(leftStack, null), PanelStackData(rightStack, null))
 
     //when
     QuickViewPlugin.onKeyTrigger("", stacks).toFuture.map(_ shouldBe js.undefined)
+
+    //then
+    stackState.length shouldBe 1
+    stackState.head.component shouldBe "fsComp"
   }
   
   it should "remove plugin from right panel when onKeyTrigger" in {
     //given
-    val leftStack = new Stack(isActive = true)
-    val rightStack = new Stack
-    val stacks = PanelStacks(PanelStackData(leftStack.stack, null), PanelStackData(rightStack.stack, null))
-    leftStack.peek.expects().returning(PanelStackItem("other".asInstanceOf[ReactClass]))
-    rightStack.peek.expects().returning(PanelStackItem(QuickViewPanel()))
-    
-    //then
-    rightStack.pop.expects()
+    var stackState = js.Array[PanelStackItem[_]](
+      PanelStackItem(QuickViewPanel(), js.undefined, js.undefined, QuickViewParams()),
+      PanelStackItem("fsComp".asInstanceOf[ReactClass], js.undefined, js.undefined, js.undefined)
+    )
+    val leftStack = new PanelStack(isActive = true, js.Array(
+      PanelStackItem("fsComp".asInstanceOf[ReactClass], js.undefined, js.undefined, js.undefined)
+    ), null)
+    val rightStack = new PanelStack(isActive = false, stackState, { f =>
+      stackState = f(stackState)
+    }: js.Function1[js.Array[PanelStackItem[_]], js.Array[PanelStackItem[_]]] => Unit)
+    val stacks = PanelStacks(PanelStackData(leftStack, null), PanelStackData(rightStack, null))
 
     //when
     QuickViewPlugin.onKeyTrigger("", stacks).toFuture.map(_ shouldBe js.undefined)
+
+    //then
+    stackState.length shouldBe 1
+    stackState.head.component shouldBe "fsComp"
   }
   
   it should "add plugin to left panel when onKeyTrigger" in {
     //given
-    val leftStack = new Stack
-    val rightStack = new Stack(isActive = true)
-    val stacks = PanelStacks(PanelStackData(leftStack.stack, null), PanelStackData(rightStack.stack, null))
-    leftStack.peek.expects().returning(PanelStackItem("other1".asInstanceOf[ReactClass]))
-    rightStack.peek.expects().returning(PanelStackItem("other2".asInstanceOf[ReactClass]))
+    var stackState = js.Array[PanelStackItem[_]](
+      PanelStackItem("fsComp".asInstanceOf[ReactClass], js.undefined, js.undefined, js.undefined)
+    )
+    val leftStack = new PanelStack(isActive = false, stackState, { f =>
+      stackState = f(stackState)
+    }: js.Function1[js.Array[PanelStackItem[_]], js.Array[PanelStackItem[_]]] => Unit)
+    val rightStack = new PanelStack(isActive = true, js.Array(
+      PanelStackItem("fsComp".asInstanceOf[ReactClass], js.undefined, js.undefined, js.undefined)
+    ), null)
+    val stacks = PanelStacks(PanelStackData(leftStack, null), PanelStackData(rightStack, null))
     
-    //then
-    leftStack.push.expects(*).onCall { resItem: PanelStackItem[_] =>
-      PanelStackSpec.assertPanelStackItem(resItem, PanelStackItem(QuickViewPanel(), js.undefined, js.undefined, QuickViewParams()))
-      ()
-    }
-
     //when
     QuickViewPlugin.onKeyTrigger("", stacks).toFuture.map(_ shouldBe js.undefined)
+    
+    //then
+    stackState.length shouldBe 2
+    stackState.head.component shouldBe QuickViewPanel()
+    stackState.head.state.toOption.value shouldBe QuickViewParams()
   }
   
   it should "add plugin to right panel when onKeyTrigger" in {
     //given
-    val leftStack = new Stack(isActive = true)
-    val rightStack = new Stack
-    val stacks = PanelStacks(PanelStackData(leftStack.stack, null), PanelStackData(rightStack.stack, null))
-    leftStack.peek.expects().returning(PanelStackItem("other1".asInstanceOf[ReactClass]))
-    rightStack.peek.expects().returning(PanelStackItem("other2".asInstanceOf[ReactClass]))
-    
-    //then
-    rightStack.push.expects(*).onCall { resItem: PanelStackItem[_] =>
-      PanelStackSpec.assertPanelStackItem(resItem, PanelStackItem(QuickViewPanel(), js.undefined, js.undefined, QuickViewParams()))
-      ()
-    }
+    var stackState = js.Array[PanelStackItem[_]](
+      PanelStackItem("fsComp".asInstanceOf[ReactClass], js.undefined, js.undefined, js.undefined)
+    )
+    val leftStack = new PanelStack(isActive = true, js.Array(
+      PanelStackItem("fsComp".asInstanceOf[ReactClass], js.undefined, js.undefined, js.undefined)
+    ), null)
+    val rightStack = new PanelStack(isActive = false, stackState, { f =>
+      stackState = f(stackState)
+    }: js.Function1[js.Array[PanelStackItem[_]], js.Array[PanelStackItem[_]]] => Unit)
+    val stacks = PanelStacks(PanelStackData(leftStack, null), PanelStackData(rightStack, null))
 
     //when
     QuickViewPlugin.onKeyTrigger("", stacks).toFuture.map(_ shouldBe js.undefined)
+
+    //then
+    stackState.length shouldBe 2
+    stackState.head.component shouldBe QuickViewPanel()
+    stackState.head.state.toOption.value shouldBe QuickViewParams()
   }
 }
